6abdc67b2cdc7bee17873d07d22827983520a2a4
mttwhrrs <mttwhrrs@gmail.com>
2025-01-12 11:54:50 -0500

    created ok_counties object

--- first.R
+++ first.R
@@ -1,3 +1,15 @@
 #code for loop to create separate files for each county in Oklahoma
 
-ok <- 
+#create object
+
+ok <- get_acs(
+geography = "county",
+state = "OK",
+variables = "B19013_001",
+year = 2020,
+)
+
+ok_counties <- ok |>
+select(GEOID, NAME)
+
+ok_counties
